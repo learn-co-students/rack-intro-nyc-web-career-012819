2af8c0474f8473facf4de9350a46a2068368aa56
stomg7969 <stomg7969@gmail.com>
2019-02-17 15:20:12 -0500

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Nate, Let's all have some Pizza for tonight!"
     resp.finish
   end
 
 end
-
