b0d095978ad3a5e4222a5111547e0e84239015ef
Farhan <farhanmahmoodny@gmail.com>
2019-02-16 23:15:20 -0500

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Farhan Mahmood."
     resp.finish
   end
 
 end
-
